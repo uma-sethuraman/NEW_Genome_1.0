af61c24092d728f8ddbba3910bd8bf474d2a9c1a
caovicto <caovicto@msu.edu>
2020-06-19 11:51:18 -0400

    moved handler out of class, added member variable GeneSegment, and getter functions to GeneSegment

--- code/GenomeLite.h
+++ code/GenomeLite.h
@@ -14,6 +14,7 @@
 #include <iostream>
 
 #include "AbstractGenome.h"
+#include "SegmentList.h"
 #include "SegmentNode.h"
 
 typedef char Byte; // c++17 std::byte doesn't always work
@@ -23,175 +24,26 @@ typedef char Byte; // c++17 std::byte doesn't always work
 class GenomeLite
 {
 private:
-	std::shared_ptr<AbstractGenome> ParentGenome; ///< pointer to parent genome
-    std::shared_ptr<SegmentNode> StartSegment; ///< start to linked list of segments
+    /// Gives handler access to variables
+    friend class Handler; 
 
-    size_t Size;   ///< size of genome
+	std::shared_ptr<AbstractGenome> ParentGenome; ///< pointer to parent genome
+    std::unique_ptr<SegmentList> GeneSegments;  ///< list of gene segments
 
 public:
     /** Constructor
      * \param  genome Parent genome the GenomeLite is constructed from*/
 	GenomeLite(std::shared_ptr<AbstractGenome> genome) 
-        : ParentGenome(genome), Size(genome->size())
-    {
-        std::cout << "GenomeLite " << std::endl;
-        std::cout << "start " << std::hex << (unsigned long)genome->data() << std::endl;
-        std::cout << "end " << (unsigned long)genome->data()+genome->size() << std::endl;
-        std::cout << std::endl;
-
-        StartSegment = std::make_shared<SegmentNode>(genome->data(), genome->size());
-    }
+        : ParentGenome(genome), GeneSegments(std::make_unique<SegmentList>(genome)) {}
 
     /** Deconstructor **/
 	~GenomeLite() {}
 
     /** Gets size 
      * \returns size of genome **/
-    size_t size() { return Size; }
+    size_t size() { return GeneSegments->siteCount(); }
 
-
-    /** Handler Handler for mutating over genome **/
-    class Handler
-    {
-    private:
-        GenomeLite* Genome; ///< current genome iterating on
-        std::shared_ptr<SegmentNode> Segment;    ///< current SegmentNode iterating on
-        size_t Pos;  ///< position up until Segment
-
-        SegmentNode::Iter SegmentIterator;
-
-    public:
-        /** (deleted) default Constructor **/
-        Handler() = delete;
-
-        /** Constructor
-         * \param genome Genome Mutation on */
-        Handler(GenomeLite* genome, size_t pos) 
-            : Genome(genome), Segment(genome->StartSegment), Pos(pos), SegmentIterator(genome->StartSegment->begin())
-        {}
-
-        /** Deconstructor **/
-        ~Handler() {}
-
-        /** Gets current position
-        * \return position in segment **/
-        const size_t index()
-        {
-            return Pos+SegmentIterator.pos();
-        }
-
-
-        /** Connect two nodes
-         * \param left node 
-         * \param right node **/
-        void Connect(std::shared_ptr<SegmentNode> left, std::shared_ptr<SegmentNode> right)
-        {
-            if (left)
-                left->SetNext(right);
-            if (right)
-                right->SetPrev(left);
-        }
-
-        /** Insertion mutation 
-         * \param startMutation address of insertion mutation **/
-        void PointMutation(Byte* startMutation)
-        {
-
-        }
-
-
-        /** Insertion mutation 
-         * \param startMutation address of insertion mutation
-         * \param sizeMutation size of mutation **/
-        void InsertMutation(Byte* startMutation, size_t sizeMutation)
-        {
-
-        }
-
-        /** Copy mutation 
-         * \param insertionMutation address of insertion mutation
-         * \param sizeMutation size of mutation **/
-        void CopyMutation(Byte* startMutation, size_t sizeMutation)
-        {
-
-        }
-
-
-        /** Insertion mutation 
-         * \param sizeMutation size of mutation **/
-        void DeleteMutation(size_t sizeMutation = 1)
-        {
-            std::cout << "delete mutation" << std::endl;
-            /// cut segment and remove value
-            auto cutSegments = Segment->Subdivide(SegmentIterator.pos());
-            auto firstSegment = cutSegments.first;
-            auto secondSegment = cutSegments.second;
-            secondSegment->TruncateLeft(sizeMutation);
-
-            /// connect the cut segments
-            Connect(Segment->prev(), firstSegment);
-            Connect(firstSegment, secondSegment);
-            Connect(secondSegment, Segment->next());
-
-            /// move head of reader to newly cut segment
-            Segment = secondSegment;
-            Pos += firstSegment->size();
-            SegmentIterator = secondSegment->begin();
-
-            /// edit genome properties
-            Genome->Size -= sizeMutation; 
-            if (Segment == Genome->StartSegment) 
-                Genome->StartSegment = firstSegment;
-        }
-
-        /** Get value at current position
-         * \returns Value at Pos in the collection */
-        Byte operator *() const 
-        { 
-            return *SegmentIterator; 
-        }
-
-        /** Test for end of the iterator
-        * \param other Other Iter object to compare against
-        * \returns True if we this position equals not equal to the other position */
-        bool operator==(const Handler& other) const
-        {
-            return Pos == other.Pos && Segment == other.Segment;
-        }
-
-
-        /** Increment the mutator
-         * \returns Reference to this mutator */
-        const Handler& operator++(int)
-        {
-            SegmentIterator++;
-            if (SegmentIterator == Segment->end())
-            {
-                Pos += Segment->size();
-                Segment = Segment->next();
-                SegmentIterator = Segment->begin();
-            }
-            return *this;
-        }
-
-        /** Increment the mutator
-         * \returns Reference to this mutator */
-        const Handler& operator--(int)
-        {
-            SegmentIterator++;
-            if (SegmentIterator == Segment->end())
-            {
-                Pos -= Segment->size();
-                Segment = Segment->prev();
-                SegmentIterator = Segment->end()--;
-            }
-            return *this;
-        }
-    };
-
-    /** Get an iterator for the beginning of the collection
-     * \returns Iter object at position 0 */
-    Handler begin() { return Handler(this, 0); }
-
-  
+    /** Gets size 
+     * \returns size of genome **/
+    size_t segmentsCount() { return GeneSegments->size(); }
 };
