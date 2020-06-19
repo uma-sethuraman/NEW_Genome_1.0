c1375cae5133d1142a1b2cc0fc28719a4d636654
caovicto <caovicto@msu.edu>
2020-06-19 12:01:11 -0400

    misc compiling

--- code/main.cpp
+++ code/main.cpp
@@ -4,57 +4,20 @@
  **/
 
 #include <iostream>
-#include <cassert>
-#include <array>
-#include <vector>
-#include <bitset>
-#include <cstddef>
-#include <memory>
 
-#include "utilities.h"
-#include "AbstractGenome.h"
-#include "Genome.h"
-#include "GenomeLite.h"
+#include "testing/HandlerTest.h"
 
 typedef char Byte; // c++17 Byte doesn't always work
 
+
 /** main testing **/
 int main() {
-	/// creating genome
-	std::shared_ptr<AbstractGenome> genome = std::make_shared<Genome>(8);
+	/// Handler Testing
+	TestHandler::testPointMutation();
+	TestHandler::testInsertionMutation();
+	TestHandler::testCopyMutation();
+	TestHandler::testDeleteMutation();
 
-	///init genome
-	for (size_t i = 0; i < genome->size(); i++)
-	{
-		genome->data()[i] = i;
-	}
-
-	std::cout << "main " << std::endl;
-	std::cout << std::endl;
-
-	GenomeLite genomeMutation(genome);
-
-	auto handler = genomeMutation.begin();
-	while (handler.index() < genomeMutation.size())
-	{
-		std::cout << handler.index() << " " << (unsigned long)*handler << std::endl;
-		if (handler.index() == (size_t)5)
-		{
-			handler.DeleteMutation();
-		}		
-		handler++;
-	}
-	
-	std::cout << "test cutting " << std::endl;
-	std::cout << std::endl;
-
-	handler = genomeMutation.begin();
-	while (handler.index() < genomeMutation.size())
-	{
-		std::cout << handler.index() << " " << (unsigned long)*handler << std::endl;
-		handler++;
-	}
-	
 
 	return(0);
 }
