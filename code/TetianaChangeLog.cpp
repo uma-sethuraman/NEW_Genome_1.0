a3abedd15ff88b8517b1adcd2ffbd70d65ababd9
tetianadadakova <tetiana.d@gmail.com>
2020-06-19 10:10:48 -0700

    Add calculate_ind() funtion

--- code/TetianaChangeLog.cpp
+++ code/TetianaChangeLog.cpp
@@ -14,10 +14,24 @@ void delete_mutation(int ind, std::vector<int>& genome, std::vector<int>& mutati
     
 }
 
+void point_mutation(int ind, std::vector<int>& genome, std::vector<int>& mutations) {
+    
+    
+}
+
 void reconstruct(std::vector<int>& genome, std::vector<int>& mutations) {
     for (auto it = mutations.begin(); it != mutations.end(); ++it) {
         genome.erase(genome.begin() + (*it)); // (*it) it index to erase
     }
+}
+
+int calculate_ind(int ind_pre_mut, std::vector<int>& mutations) {
+    // Returns correct index in the parent genome based on index in current genome array and change log (mutations)
+    
+    int before_inds_num = count_if (mutations.begin(), mutations.end(), [ind_pre_mut](int i){ return i <= ind_pre_mut; });
+    int ind_post_mut = ind_pre_mut + before_inds_num;
+    
+    return ind_post_mut;
 }
 
 
@@ -38,9 +52,18 @@ int main()
     std::cout << '\n';
     
     delete_mutation(2, genome, mutations);
+    std::cout << "Index 0 is now " << calculate_ind(0, mutations) << std::endl;
+    std::cout << "Index 2 is now " << calculate_ind(2, mutations) << std::endl;
+    std::cout << "Index 6 is now " << calculate_ind(6, mutations) << std::endl;
     delete_mutation(3, genome, mutations);
     delete_mutation(6, genome, mutations);
+    std::cout << "Index 0 is now " << calculate_ind(0, mutations) << std::endl;
+    std::cout << "Index 2 is now " << calculate_ind(2, mutations) << std::endl;
+    std::cout << "Index 6 is now " << calculate_ind(6, mutations) << std::endl;
     delete_mutation(4, genome, mutations);
+    std::cout << "Index 0 is now " << calculate_ind(0, mutations) << std::endl;
+    std::cout << "Index 2 is now " << calculate_ind(2, mutations) << std::endl;
+    std::cout << "Index 6 is now " << calculate_ind(6, mutations) << std::endl;
     
     //if (first_deletion != genome.end())
     //    std::cout << "first_deletion: " << *first_deletion << std::endl;
