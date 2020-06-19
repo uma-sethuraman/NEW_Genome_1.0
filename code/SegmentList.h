d593a49bbba0d74cf9adef58f235f9a3d2fc67c6
caovicto <caovicto@msu.edu>
2020-06-19 12:00:37 -0400

    created container for SegmentNodes

---
+++ code/SegmentList.h
@@ -0,0 +1,51 @@
+/**
+ * \file SegmentList.h
+ * \author Victoria Cao
+ * 
+ * \brief Class for keeping track of a linked list of 
+ * Segment NOdes
+ **/
+
+#pragma once
+#include <cstdint>
+#include <cstddef>
+#include <memory>
+#include <iostream>
+
+#include "AbstractGenome.h"
+#include "SegmentNode.h"
+
+typedef char Byte; // c++17 std::byte doesn't always work
+
+
+/** Implementation of a Segment List object **/
+class SegmentList
+{
+private:
+    /// Gives handler access to variables
+    friend class Handler; 
+
+    /// Data variables
+    size_t Size = 1; ///< number of nodes in list
+    size_t SiteCount;   ///< sum of sites from all nodes
+
+    /// Linked list variables
+    std::shared_ptr<SegmentNode> StartSegment;
+
+public:
+    /** (deleted) Default constructor **/
+    SegmentList() = delete;
+
+    /** Constructor 
+     * \param genome to construct from **/
+    SegmentList(std::shared_ptr<AbstractGenome> genome) 
+        :  SiteCount(genome->size()), StartSegment(std::make_shared<SegmentNode>(genome->data(), genome->size())) {}
+
+    /** gets number of segments in list
+     * \return size **/
+    const size_t size() { return Size; }
+    /** gets total number of sites from all segments
+     * \return siteCount **/
+    const size_t siteCount() { return SiteCount; }
+    
+};
