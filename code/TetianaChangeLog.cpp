792a9d399595ddd6d004f4b99e631efae3a4e9c2
tetianadadakova <tetiana.d@gmail.com>
2020-06-18 20:55:00 -0700

    Simplify delete_mutation() and update reconstruct()

--- code/TetianaChangeLog.cpp
+++ code/TetianaChangeLog.cpp
@@ -5,65 +5,30 @@
 #include <algorithm>
 #include <memory>
 
-void delete_mutation(int ind, std::vector<int>& genome, std::set<int>& mutations, 
-    std::vector<int>::iterator& first_deletion) {
+
+void delete_mutation(int ind, std::vector<int>& genome, std::vector<int>& mutations) {
     
-    // TODO check if index is outside of boundaries
-    
-    if (first_deletion == genome.end()) { // first deletion
-        first_deletion = genome.begin() + ind; //std::vector<int>::iterator to_del_it = genome.begin() + ind;
-    } else {
-        if (ind > (first_deletion - genome.begin())) { // index to delete is after first_deletion
-            if (mutations.find(ind) != mutations.end()) {
-                // already exists - need to delete next one
-                mutations.insert(ind + 1); // what is ind + 1 also in the set
-            } else {
-                // update
-                mutations.insert(ind);
-            }
-                
-            
-        } else if (ind < (first_deletion - genome.begin())) {
-            std::vector<int>::iterator to_del_it = genome.begin() + ind;
-            mutations.insert(*first_deletion);
-            first_deletion = to_del_it;
-        }  else {
-            // if ind == (first_deletion - genome.begin()) - need to delete next one
-            if (mutations.find(ind + 1) != mutations.end()) {
-                // already exists - need to delete next one
-                mutations.insert(ind + 2);
-            } else {
-                // update
-                mutations.insert(ind + 1);
-            }
-        }
-        
-    }
+    int before_inds_num = count_if (mutations.begin(), mutations.end(), [ind](int i){ return i < ind; });
+    mutations.push_back(ind - before_inds_num);
+    std::cout << "pushing: " << ind - before_inds_num << std::endl;
     
 }
 
-void reconstruct(std::vector<int>& genome, std::set<int>& mutations, 
-    std::vector<int>::iterator first_deletion) {
-    
-    if (first_deletion != genome.end()) {
-        for (auto it = mutations.rbegin(); it != mutations.rend(); ++it) {
-            genome.erase(/*first_deletion*/ genome.begin() + (*it)); // iterator + int
-        }
-        genome.erase(first_deletion); // iterator
+void reconstruct(std::vector<int>& genome, std::vector<int>& mutations) {
+    for (auto it = mutations.begin(); it != mutations.end(); ++it) {
+        genome.erase(genome.begin() + (*it)); // (*it) it index to erase
     }
-    
-    
 }
 
 
 int main()
 {
-    std::vector<int> genome{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int> genome{0, 1, 2, 3, 4, 5, 6};
     
     //std::set<std::vector<int>::iterator> mutations;
     //std::unique_ptr<int> first_deletion = nullptr;
-    std::vector<int>::iterator first_deletion = genome.end();
-    std::set<int> mutations;
+    //std::vector<int>::iterator first_deletion = genome.end();
+    std::vector<int> mutations;
     
     //==============
     auto print = [](const int& n) { std::cout << " " << n; };
@@ -72,9 +37,10 @@ int main()
     std::for_each(genome.cbegin(), genome.cend(), print);
     std::cout << '\n';
     
-    delete_mutation(1, genome, mutations, first_deletion);
-    delete_mutation(3, genome, mutations, first_deletion);
-    delete_mutation(2, genome, mutations, first_deletion);
+    delete_mutation(2, genome, mutations);
+    delete_mutation(3, genome, mutations);
+    delete_mutation(6, genome, mutations);
+    delete_mutation(4, genome, mutations);
     
     //if (first_deletion != genome.end())
     //    std::cout << "first_deletion: " << *first_deletion << std::endl;
@@ -83,7 +49,7 @@ int main()
     std::for_each(genome.cbegin(), genome.cend(), print);
     std::cout << '\n';
     
-    reconstruct(genome, mutations, first_deletion);
+    reconstruct(genome, mutations);
     
     std::cout << "genome after reconstruction:\n";
     std::for_each(genome.cbegin(), genome.cend(), print);
