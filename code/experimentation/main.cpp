a9d5eb8db2ed66f4dd47363f3cdd9fdab2caa07d
caovicto <caovicto@msu.edu>
2020-06-25 15:00:40 -0400

    new build and run for tree

---
+++ code/experimentation/main.cpp
@@ -0,0 +1,18 @@
+/** 
+ * \file main.cpp
+ * \author Victoria Cao
+ **/
+
+#include <iostream>
+#include "testing/TestTree.h"
+
+typedef char Byte; // c++17 Byte doesn't always work
+
+
+/** main testing **/
+int main() {
+	/// Handler Testing
+	TestTree::TestDelete();
+
+	return(0);
+}
