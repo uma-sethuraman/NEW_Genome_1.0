21698aeb43ab1e58dec95e31f64d2a08ec0eb714
caovicto <caovicto@msu.edu>
2020-07-06 13:09:10 -0400

    Created genomeLite base functions

---
+++ code/Genome/GenomeLite/GenomeLite.cpp
@@ -0,0 +1,15 @@
+/**
+ * \file GenomeLite.cpp
+ * \author Victoria Cao
+ **/
+
+#include <iostream>
+
+#include "GenomeLite.h"
+
+
+/** Reallocates to new tree once mutations are too different
+**/
+void GenomeLite::Reallocate()
+{
+}
---
+++ code/Genome/GenomeLite/GenomeLite.h
@@ -0,0 +1,46 @@
+/**
+ * \file GenomeLite.h
+ * \author Victoria Cao
+ * 
+ * \brief Base class for a simple GenomeLite class using SegmentNode 
+ * implementation
+ **/
+
+#pragma once
+#include <cstdint>
+#include <iostream>
+
+#include "../AbstractHandler.h"
+#include "../AbstractGenome.h"
+#include "SegmentTree.h"
+
+
+typedef char Byte; // c++17 std::byte doesn't always work
+
+
+/** Implementation of a genomeLite object **/
+class GenomeLite : public AbstractGenome
+{
+private:
+    SegmentTree* Tree;
+
+    void Reallocate();
+
+public:
+    /** (deleted) default constructor **/
+    GenomeLite() = delete;
+
+    /** size constructor **/
+    GenomeLite(size_t size) : AbstractGenome(size) {}
+
+    /** Gets size of genome sites
+     * \return size of tree **/
+    virtual size_t size() { return Tree->GetSiteCount(); }
+
+    virtual void initalize() override;
+    virtual void overwrite(size_t index, std::vector<std::byte> segment) override;
+    virtual void insert(size_t index, std::vector<std::byte> segment) override;
+    virtual void remove(size_t index, size_t segmentSize) override;
+
+
+};
