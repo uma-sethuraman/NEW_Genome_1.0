07765e91f93b88acc060e5300b9fcfaa76783660
caovicto <caovicto@msu.edu>
2020-07-17 11:23:16 -0400

    created benching for basic mutations

---
+++ code/Bench/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# compile
+cmake CMakeLists.txt && make
+
+# move out of file
+cd ../
+
+# clean up compilation files
+find . -name "cmake_install.cmake" -type f -delete
+find . -name "CMakeCache.txt" -type f -delete
+find . -name "Makefile" -type f -delete
+find . -name "libgenome.a" -type f -delete
+find . -type d -name "CMakeFiles" -exec rm -rf {} \;
