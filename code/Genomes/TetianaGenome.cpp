e8e8f2c4c63027fc7eeab03756dea49ad05c961a
tetianadadakova <tetiana.d@gmail.com>
2020-07-28 11:51:59 -0700

    Clean up code

--- code/Genomes/TetianaGenome.cpp
+++ code/Genomes/TetianaGenome.cpp
@@ -49,33 +49,36 @@ std::byte* TetianaGenome::data(size_t index, size_t byteSize) {
 }
 
 int TetianaGenome::get_ind(int ind_curr) {
+    // Get the index in the parent genome, which correspond to the index in the offspring genome
+    // This funciton is only called for remove mutations, because the inserted indexes are not the in the oarent
+    
+    // Find key, which is <= index
     auto it_curr = --change_log.upper_bound(ind_curr);
-    if (it_curr->second.second == true) { // insertion
-                                          // TODO: do I really need this check?
-        std::cout << "get_ind doesn't work for inserted values" << std::endl;
-        return -1;
-    }
-    else { // deletion
-        return ind_curr - it_curr->second.first;
-    }
+    return ind_curr - it_curr->second.first;
 }
 
 std::byte TetianaGenome::val_at_index(int ind_curr) {
+    // Find key, which is <= index
     auto it_curr = --change_log.upper_bound(ind_curr);
     
-    if (it_curr->second.second == true) { // insertion
+    if (it_curr->second.second == true) { // was inserted
         int inserted_ind = ind_curr - it_curr->first;
         return (segments_log.at(it_curr->first))[inserted_ind];
     }
     
-    int parent_ind = ind_curr - it_curr->second.first;
+    // was removed
+    int parent_ind = ind_curr - it_curr->second.first; // = get_ind(int ind_curr);
     return sites[parent_ind];
 }
 
 void TetianaGenome::resize(size_t new_size) {
+    // TODO: it doesn't have to be collapsed
+    
+    std::vector<std::byte> offspring_sites = offspring_recon(0, 0);
+    sites = offspring_sites;
+    
     sites.resize(new_size);
     
-    // TODO: this has to be rewritten
     change_log.clear();
     change_log[0] = {0, false};
     segments_log.clear();
@@ -84,22 +87,10 @@ void TetianaGenome::resize(size_t new_size) {
 };
 
 AbstractGenome* TetianaGenome::clone(bool forceCopy) {
+    // TODO: I have no idea what I'm doing here
+
     if (forceCopy == true) {
-        
-        // do I need this?
-        //        if (change_log.size() == 1) {
-        //            if (change_log.begin()->first == 0 && change_log.begin()->second.first == 0 && change_log.begin()->second.second == false) {
-        //                // no mutations, sites is already in memory
-        //                return sites;
-        //            }
-        //        }
-        
         std::vector<std::byte> offspring_sites = offspring_recon(0, 0);
-//        std::cout << "offspring: " << std::endl;
-//        for (auto el : offspring_sites) {
-//            std::cout << (int)el << " ";
-//        }
-//        std::cout << std::endl;
         
         // Genome clone
         // Will this init empty change_log and segments_log?
@@ -109,27 +100,24 @@ AbstractGenome* TetianaGenome::clone(bool forceCopy) {
         return cloned;
     }
     
-//    std::cout << "offspring if not forced clone: " << std::endl;
-//    for (auto el : this->sites) {
-//        std::cout << (int)el << " ";
-//    }
-//    std::cout << std::endl;
     return new TetianaGenome(*this);
 }
 
 
-// starting at index, write values in segement over values currently in genome
 void TetianaGenome::overwrite(size_t index, const std::vector<std::byte>& segment) {
     
     for (int ind = 0; ind < segment.size(); ++ind) {
         
-        auto it_next = change_log.upper_bound(ind + index);
-        auto it_prev = std::prev(it_next);
+        // TODO: this is not optimal, because O(logN) every iteration
+        auto it_next = change_log.upper_bound(ind + index); // O(logN)
+        auto it_prev = std::prev(it_next); // O(1)
         
         if (it_prev->second.second == true) {
-            (segments_log[it_prev->first])[(index + ind) - it_prev->first] = segment[ind];
+            // update in segments_log
+            (segments_log[it_prev->first])[(index + ind) - it_prev->first] = segment[ind]; // O(1)
         } else {
             // update in parent genome
+            // TODO: this is actually wrong, parent genome has to stay intact
             int ind_orig = get_ind((ind + index));
             sites[ind_orig] = segment[ind];
         }
@@ -139,7 +127,7 @@ void TetianaGenome::overwrite(size_t index, const std::vector<std::byte>& segmen
 
 void TetianaGenome::remove(size_t index, size_t segmentSize) {
     
-    std::cout << "===== remove " << index << ", " << segmentSize <<  " =====" << std::endl;
+//    std::cout << "===== remove " << index << ", " << segmentSize <<  " =====" << std::endl;
     
     if (index + segmentSize > genomeSize) {
         std::cout << "attept to erase would erase past end of genome! exiting..." << std::endl;
@@ -306,14 +294,15 @@ void TetianaGenome::insert(size_t index, const std::vector<std::byte>& segment) 
     // Temporary maps that will be updated
     // Need to use temporary maps, because as I iterate through map to update it,
     // the iterators become invalid
+    // TODO: use reverse iterators
     std::map<int, std::pair<int, bool>> change_log_temp;
     std::unordered_map<int, std::vector<std::byte>> segments_log_temp;
     
-    if (prev_key->second.second == true) { // prev_key == lb_key is key == index
+    if (prev_key->second.second == true) { // prev_key == lb_key if key == index
         
         // Keys before insertion
         for (auto it = change_log.begin(); it != std::next(prev_key); ++it) { // lb_key instead of std::next(prev_key)?
-                                                                              // update change_log
+            // update change_log
             change_log_temp.insert(std::make_pair(it->first, std::make_pair(it->second.first, it->second.second)));
             
             // update segments_log
@@ -376,13 +365,10 @@ void TetianaGenome::insert(size_t index, const std::vector<std::byte>& segment) 
         }
         
         change_log_temp[index] = {0, true}; // want to replace if exists
-                                            //change_log_temp.insert({{index + segmentSize, {prev_key->second.first + segmentSize, false}}});
         change_log_temp[index + segmentSize] = {prev_key->second.first + segmentSize, false};
-        
         segments_log_temp.insert({{index, segment}});
         
         // in this situation two inserted segments will never have to merge
-        
         segments_log = segments_log_temp;
     }
     change_log = change_log_temp; // is this constant time? would std::move make it constant time?
@@ -394,20 +380,10 @@ std::vector<std::byte> TetianaGenome::offspring_recon(size_t index, size_t byteS
         return std::vector<std::byte>{};
     }
     
-    
-    //    // Calculte size of the offspring genome .rbegin() will not return .rend(), because log is never empty
-    //    int offspr_sites_size = sites.size() + change_log.rbegin()->second.first; // last key's val
-    //    if (offspr_sites_size != genomeSize) {
-    //        std::cout << "GENOME SIZE IS WRONG!" << std::endl;
-    //        exit(1);
-    //    }
-    
-    
     int sites_offspring_size = 0;
     if (byteSize == 0) sites_offspring_size = genomeSize;
     else sites_offspring_size = byteSize;
     
-    //std::vector<std::byte> sites_offspring(sites_offspring_size, (std::byte)0);
     std::vector<std::byte> sites_offspring(genomeSize, (std::byte)0);
     
     for (auto it_curr = change_log.begin(); it_curr != change_log.end(); ++it_curr) {
@@ -415,19 +391,11 @@ std::vector<std::byte> TetianaGenome::offspring_recon(size_t index, size_t byteS
         
         if (it_curr->second.second == true) {
             std::vector<std::byte> vals_to_insert = segments_log.at(it_curr->first);
-//            if (it_next == change_log.end()) { // will this ever happen? - No!
-//                                               // This loop fills out the last part of the vector
-//                int ind_to_insert = 0;
-//                for (int ind = it_curr->first; ind < genomeSize; ++ind) {
-//                    sites_offspring[ind] = vals_to_insert[ind_to_insert];
-//                    ind_to_insert++;
-//                }
-//            } else {
-                int ind_to_insert = 0;
-                for (int ind = it_curr->first; ind < it_next->first; ++ind) {
-                    sites_offspring[ind] = vals_to_insert[ind_to_insert];
-                    ind_to_insert++;
-//                }
+            
+            int ind_to_insert = 0;
+            for (int ind = it_curr->first; ind < it_next->first; ++ind) {
+                sites_offspring[ind] = vals_to_insert[ind_to_insert];
+                ind_to_insert++;
             }
         } else {
             if (it_next == change_log.end()) {
@@ -445,19 +413,17 @@ std::vector<std::byte> TetianaGenome::offspring_recon(size_t index, size_t byteS
         }
     }
     
-    // We are not using these in tests yet
+    // TODO: This is not optimal, but we are not using these in tests yet
     auto start = sites_offspring.begin() + index;
     auto end = sites_offspring.begin() + (index + sites_offspring_size);
     std::vector<std::byte> sites_offspring_part(start, end);
     
-//    std::cout << "sites_offspring.size(): " << sites_offspring.size() << std::endl;
-//    std::cout << "sites_offspring_part.size(): " << sites_offspring_part.size() << std::endl;
-
-    //return sites_offspring;
     return sites_offspring_part;
 }
 
 void TetianaGenome::show() {
+    
+    // TODO: Is it better to instead call val_at_ind that collapse the genome?
     
     if (change_log.size() == 1 && (change_log.begin()->first == 0 && change_log.begin()->second.first == 0 && change_log.begin()->second.second == false)) {
         // no mutations
