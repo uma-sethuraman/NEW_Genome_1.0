f604ec72b6dfe350f9b7a422013cfa80cbeddaaf
Uma Sethuraman <uma.sethuraman@utexas.edu>
2020-08-16 12:10:44 -0500

    Updating test suites in main to run by default on TestGenome

--- code/Test/main.cpp
+++ code/Test/main.cpp
@@ -30,22 +30,22 @@ int main()
     /* Pass in false(0) to just see pass/fail results of all tests.
        Pass in true(1) to see pass/fail results AND debug output of all tests.
        Pass in <GenomeName> based on which genome class you want to test. */
-    UmaTests::runAllTests<UmaGenome>(0);
+    UmaTests::runAllTests<TestGenome>(0);
 
     /**     Victoria's Testing Suite         **/
     /* Pass in false(0) to just see pass/fail results of all tests.
        Pass in true(1) to see pass/fail results AND debug output of all tests.
        Pass in <GenomeName> based on which genome class you want to test. */
-     VictoriaTests::TestAll<UmaGenome>(0);
+     VictoriaTests::TestAll<TestGenome>(0);
 
     /**     Invalid Mutations Test Suite     **/
     /* The tests below test the out of bounds checks in the mutation methods.
        Run each test by itself, not all three together, since each test should exit.
        Pass in <GenomeName> based on which genome class you want to test.
        Pass in true(1) to enable debug mode. */
-    InvalidMutationTests::invalidOverwriteTest<UmaGenome>(0);
-    //InvalidMutationTests::invalidInsertTest<UmaGenome>(0);
-    //InvalidMutationTests::invalidRemoveTest<UmaGenome>(0);
+    InvalidMutationTests::invalidOverwriteTest<TestGenome>(0);
+    //InvalidMutationTests::invalidInsertTest<TestGenome>(0);
+    //InvalidMutationTests::invalidRemoveTest<TestGenome>(0);
 
     return(0);
 }
